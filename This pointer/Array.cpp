63864ad4ec4803a23339f123156b36bbafcf6f58
FYR666 <31131580+FYR666@users.noreply.github.com>
2018-08-22 20:15:59 +0800

    This指针

---
+++ This pointer/Array.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include "Array.h"
+using namespace std;
+
+Array::Array(int len)
+{
+	this->len = len;
+}
+
+
+Array::~Array()
+{
+
+}
+
+void Array::setLen(int len)
+{
+	this->len = len;
+}
+
+int Array::getLen()
+{
+	return len;
+}
+
+void Array::printInfo()
+{
+
+}
---
+++ This pointer/Array.h
@@ -0,0 +1,12 @@
+class Array
+{
+public:
+	Array(int len);
+	~Array();
+	void setLen(int len);
+	int getLen();
+	void printInfo();
+private:
+	int len;
+};
+
